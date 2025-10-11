b7c9cdaf0d1ebd6561abbe143e72e36737cc8e8b
goulustis <cnimaiv@gmail.com>
2025-10-11 17:22:49 +0800

    name change

--- scripts/run_agent_server.py
+++ scripts/run_agent_server.py
@@ -2,11 +2,11 @@ import tyro
 import asyncio
 
 from lang_agent.pipeline import Pipeline, PipelineConfig
-from lang_agent.config import load_config
+from lang_agent.config import load_tyro_conf
 
 def main(conf:PipelineConfig):
     if conf.config_f is not None:
-        conf = load_config(conf.config_f)
+        conf = load_tyro_conf(conf.config_f)
     
     pipeline:Pipeline = conf.setup()
     asyncio.run(pipeline.start_server())
