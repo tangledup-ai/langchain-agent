32d8ae9b29971d81a0915bef8f61c98b0899ee6d
goulustis <cnimaiv@gmail.com>
2025-10-13 21:11:42 +0800

    add sanity

--- scripts/start_mcp_server.py
+++ scripts/start_mcp_server.py
@@ -2,11 +2,19 @@ import tyro
 
 from lang_agent.mcp_server import MCPServerConfig, MCPServer
 
+# NOTE: some sanity check
+# async def main(conf:MCPServerConfig):
+#     server: MCPServer = conf.setup()
+#     u = await server.mcp._mcp_call_tool("retrieve", {"query":"test"})
+#     print(u)
+
+# import asyncio
+    # asyncio.run(main(tyro.cli(MCPServerConfig)))
+
 
 def main(conf:MCPServerConfig):
     server: MCPServer = conf.setup()
     server.run()
-
 
 if __name__ == "__main__":
     main(tyro.cli(MCPServerConfig))
