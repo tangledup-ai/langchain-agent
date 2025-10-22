3a150d6b8d33044aac8a168617fde53e6904233d
goulustis <cnimaiv@gmail.com>
2025-10-22 12:29:54 +0800

    add graph base

--- lang_agent/base.py
+++ lang_agent/base.py
@@ -7,3 +7,16 @@ class LangToolBase(ABC):
     @abstractmethod
     def get_tool_fnc(self)->List[Callable]:
         pass
+
+
+class GraphBase(ABC):
+    
+    @property
+    @abstractmethod
+    def agent(self):
+        """The agent object that must be provided by concrete implementations."""
+        pass
+    
+    def get_agent(self):
+        """Convenience method to access the agent."""
+        return self.agent
