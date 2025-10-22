a5464506e559fd0c0317ae33ff4113389268ed6d
goulustis <cnimaiv@gmail.com>
2025-10-22 16:37:24 +0800

    add routing

--- lang_agent/graphs/__init__.py
+++ lang_agent/graphs/__init__.py
@@ -1,9 +1,11 @@
 import tyro
 
 from lang_agent.graphs.react import ReactGraphConfig
+from lang_agent.graphs.routing import RoutingConfig
 
 graph_dict = {
-    "react": ReactGraphConfig()
+    "react": ReactGraphConfig(),
+    "route": RoutingConfig()
 }
 
 graph_union = tyro.extras.subcommand_type_from_defaults(graph_dict, prefix_names=False)
