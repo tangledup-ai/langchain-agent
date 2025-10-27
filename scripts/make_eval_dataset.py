35a01ac18f28a51a2f3b489dc98684132a2ed75d
goulustis <cnimaiv@gmail.com>
2025-10-27 16:39:54 +0800

    make dataset

---
+++ scripts/make_eval_dataset.py
@@ -0,0 +1,31 @@
+from langsmith import Client
+from loguru import logger
+
+
+DATASET_NAME = "dev_langagent"
+
+examples = [
+    {
+        "inputs": {"text": "用retrieve查询光予尘然后介绍"},
+        "outputs": {"answer": "茉莉绿茶为底，清冽茶香中漫出玫珑蜜瓜的绵甜与凤梨的明亮果香，层次鲜活；顶部白柚茉莉泡沫轻盈漫过舌尖，带着微酸的清新感，让整体风味更显灵动",
+                    "tool_use": ["retrieve"]}
+    },
+    {
+        "inputs": {"text": "介绍一下自己"},
+        "outputs": {"answer": "我是小盏，是一个点餐助手"}
+    }
+]
+
+cli = Client()
+
+try:
+    dataset = cli.read_dataset(dataset_name=DATASET_NAME)
+    logger.info("read dataset")
+except:
+    dataset = cli.create_dataset(dataset_name=DATASET_NAME)
+    logger.info("created dataset")
+
+cli.create_examples(
+    dataset_id=dataset.id,
+    examples=examples
+)
