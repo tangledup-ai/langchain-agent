f00ea2e15754d460b2b6efddedc4da3dde095af8
goulustis <cnimaiv@gmail.com>
2025-10-29 19:06:14 +0800

    save results

--- scripts/eval.py
+++ scripts/eval.py
@@ -5,6 +5,7 @@ from lang_agent.eval import Evaluator, EvaluatorConfig
 def main(conf: EvaluatorConfig):
     evaluator: Evaluator = conf.setup()
     evaluator.evaluate()
+    evaluator.save_results()
 
 
 if __name__ == "__main__":
