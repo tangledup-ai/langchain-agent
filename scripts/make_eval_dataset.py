9dd5a7d52c0e49083755dec77900954373469925
jijiahao <15202802357@163.com>
2025-11-05 20:34:24 +0800

    修改提示词、添加获取远程tool_manager.py实现获取远程mcp工具功能、添加实验数据集

--- scripts/make_eval_dataset.py
+++ scripts/make_eval_dataset.py
@@ -13,12 +13,12 @@ examples = [
     {
         "inputs": {"text": "有没有光予尘？"},
         "outputs": {"answer": "有的",
-                    "tool_use": ["retrieve|get_resource"]}
+                    "tool_use": ["retrieve|get_resources"]}
     },
     {
         "inputs": {"text": "有没有关羽尘？"},
         "outputs": {"answer": "有的",
-                    "tool_use": ["retrieve|get_resource"]}
+                    "tool_use": ["retrieve|get_resources"]}
     },
     {
         "inputs": {"text": ["我要购买一杯野星星",
@@ -47,6 +47,18 @@ examples = [
         "inputs": {"text": "你是谁？"},
         "outputs": {"answer": "小盏"}
     },
+    {
+        "inputs": {"text":"介绍下你的形象"},
+        "outputs": {"answer":"小盏是一只中式茶盖碗，名字来源半盏新青年茶馆。有个蓝色鼻子"}
+    },
+    {
+        "inputs": {"text":"介绍下你的公司"},
+        "outputs": {"answer":"叠加态 AI（TANGLED UP AI）是一家专注于 AI 技术应用的公司，由一帮名校和海归创始人创立，致力于将 AI 技术落地到实际场景中。2023年3月成立，专注于AI前沿应用拓展，是云南地区在该领域的新兴力量"}
+    },
+    {
+        "inputs": {"text":"介绍下你的品牌"},
+        "outputs": {"answer":"半盏新青年茶馆成立时间与理念：2023 年创立于云南，结合茶饮与创意生活方式，致力于解构传统茶文化，重构 “无边界的饮茶生活”，以新青年视角探索云南风物。探索云南风物的过程，我们将以新青年的视角，解构传统茶饮的魅力，重构充满创意与温度的新式茶文化。通过嗅觉、味觉、听觉乃至视觉的世界里，讲述云南的故事"}
+    }
 ]
 
 cli = Client()
