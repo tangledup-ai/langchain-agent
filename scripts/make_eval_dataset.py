62f56827b56b01a66d37712d7bbc1e8d32acf6f9
goulustis <cnimaiv@gmail.com>
2026-01-06 00:22:52 +0800

    remove duplicate

--- scripts/make_eval_dataset.py
+++ scripts/make_eval_dataset.py
@@ -44,10 +44,6 @@ examples = [
         "inputs": {"text": "有口感甘甜的茶吗?"},
         "outputs": {"answer": "有的，伏身于自然",
                     "tool_use": ["search_dishes"]}
-    },{
-        "inputs": {"text": "有没有野星星这杯茶"},
-        "outputs": {"answer": "有，野星星（2003年野生大树春茶-生普）采自云南西双版纳原始山林，历经二十余载陈化，汤感醇厚，回甘迅猛，尽显时光韵味。",
-                    "tool_use": ["search_dishes"]}
     },
     {
         "inputs": {"text": "你们这里有没有有密香的茶"},
