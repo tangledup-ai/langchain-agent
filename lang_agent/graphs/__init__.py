95b61c76959077480f106476c76b19c33b4800dc
goulustis <cnimaiv@gmail.com>
2026-01-06 14:03:20 +0800

    register dual

--- lang_agent/graphs/__init__.py
+++ lang_agent/graphs/__init__.py
@@ -2,10 +2,12 @@ import tyro
 
 from lang_agent.graphs.react import ReactGraphConfig
 from lang_agent.graphs.routing import RoutingConfig
+from lang_agent.graphs.dual_path import DualConfig
 
 graph_dict = {
     "react": ReactGraphConfig(),
-    "route": RoutingConfig()
+    "route": RoutingConfig(),
+    "dual": DualConfig(),
 }
 
 graph_union = tyro.extras.subcommand_type_from_defaults(graph_dict, prefix_names=False)
