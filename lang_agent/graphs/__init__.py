672b6c199e481c11143ddded1394b8ff73e4bfcb
goulustis <cnimaiv@gmail.com>
2026-01-15 16:02:38 +0800

    register vision routing

--- lang_agent/graphs/__init__.py
+++ lang_agent/graphs/__init__.py
@@ -3,11 +3,15 @@ import tyro
 from lang_agent.graphs.react import ReactGraphConfig
 from lang_agent.graphs.routing import RoutingConfig
 from lang_agent.graphs.dual_path import DualConfig
+from lang_agent.graphs.vision_routing import VisionRoutingConfig
+# from lang_agent.graphs.xiaoai_demo import XiaoAiConfig
 
 graph_dict = {
     "react": ReactGraphConfig(),
     "route": RoutingConfig(),
     "dual": DualConfig(),
+    "vision": VisionRoutingConfig(),
+    # "ai_demo": XiaoAiConfig()
 }
 
 graph_union = tyro.extras.subcommand_type_from_defaults(graph_dict, prefix_names=False)
