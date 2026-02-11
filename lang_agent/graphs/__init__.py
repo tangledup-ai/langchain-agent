e2b3cdf5e13c9e409c0b8c56e113a44236748fa5
goulustis <cnimaiv@gmail.com>
2026-02-11 13:15:44 +0800

    remove garbage

--- lang_agent/graphs/__init__.py
+++ lang_agent/graphs/__init__.py
@@ -4,14 +4,12 @@ from lang_agent.graphs.react import ReactGraphConfig
 from lang_agent.graphs.routing import RoutingConfig
 from lang_agent.graphs.dual_path import DualConfig
 from lang_agent.graphs.vision_routing import VisionRoutingConfig
-# from lang_agent.graphs.xiaoai_demo import XiaoAiConfig
 
 graph_dict = {
     "react": ReactGraphConfig(),
     "route": RoutingConfig(),
     "dual": DualConfig(),
-    "vision": VisionRoutingConfig(),
-    # "ai_demo": XiaoAiConfig()
+    "vision": VisionRoutingConfig()
 }
 
 graph_union = tyro.extras.subcommand_type_from_defaults(graph_dict, prefix_names=False)
