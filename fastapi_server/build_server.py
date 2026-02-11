6476ec82b0ed4a23c48e29f210e1b2bb571cc64b
goulustis <cnimaiv@gmail.com>
2026-02-11 12:59:20 +0800

    build server

--- fastapi_server/build_server.py
+++ fastapi_server/build_server.py
@@ -1,20 +1,39 @@
 from typing import List
 import subprocess
 
-def build_route(pipelin_id:str,
-                prompt_set:str,
-                tool_keys:List[str],
-                port:str,
-                entry_pnt:str="fastapi_server/server_dashscope.py",
-                llm_name:str="qwen-plus",):
+def _build_template(graph:str,
+                    pipelin_id:str,
+                    prompt_set:str,
+                    tool_keys:List[str],
+                    port:str,
+                    entry_pnt:str="fastapi_server/server_dashscope.py",
+                    llm_name:str="qwen-plus"):
+    
     sv_prc = subprocess(
         "python", entry_pnt,
         "--llm-name", llm_name,
         "--port", port,
-        "route",
+        graph,
         "--pipeline-id", pipelin_id,
         "--prompt-set-id", prompt_set,
         "--tool-manager-config.client-tool-manager.tool-keys", tool_keys,
     )
 
-    return sv_prc
+    return sv_prc, f"http://0.0.0.0:{port}"
+
+def build_route(pipelin_id:str,
+                prompt_set:str,
+                tool_keys:List[str],
+                port:str,
+                entry_pnt:str="fastapi_server/server_dashscope.py",
+                llm_name:str="qwen-plus"):
+    return _build_template("route", pipelin_id, prompt_set, tool_keys, port, entry_pnt, llm_name)
+
+
+def build_route(pipelin_id:str,
+                prompt_set:str,
+                tool_keys:List[str],
+                port:str,
+                entry_pnt:str="fastapi_server/server_dashscope.py",
+                llm_name:str="qwen-plus"):
+    return _build_template("react", pipelin_id, prompt_set, tool_keys, port, entry_pnt, llm_name)
