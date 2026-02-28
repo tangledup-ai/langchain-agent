bd4dfaad2ab1ba8729dc0beb54cf1b9dc60efc70
goulustis <cnimaiv@gmail.com>
2026-02-28 17:06:25 +0800

    rename

--- lang_agent/graphs/__init__.py
+++ lang_agent/graphs/__init__.py
@@ -5,7 +5,7 @@ from lang_agent.graphs.routing import RoutingConfig, RoutingGraph
 from lang_agent.graphs.dual_path import DualConfig, Dual
 from lang_agent.graphs.vision_routing import VisionRoutingConfig, VisionRoutingGraph
 # from lang_agent.graphs.child_demo import ChildDemoGraphConfig, ChildDemoGraph
-from lang_agent.graphs.qt_deepagents import DeepAgentConfig
+from lang_agent.graphs.deepagents_qt import DeepAgentConfig
 
 graph_dict = {
     "react": ReactGraphConfig(),
