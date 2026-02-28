b6f26c1e1b1a14ecbdfb5df38cdc7cd7fc9c0668
goulustis <cnimaiv@gmail.com>
2026-02-28 15:48:58 +0800

    add base filesystem backend

---
+++ lang_agent/fs_bkends/base.py
@@ -0,0 +1,24 @@
+from dataclasses import dataclass, field, is_dataclass
+from typing import Any
+import tyro
+import os.path as osp
+from abc import ABC, abstractmethod
+
+
+class BaseFilesystemBackend(ABC):
+    backend: Any
+
+    @abstractmethod
+    def _build_backend(self):
+        pass
+
+    def get_backend(self):
+        return self.backend
+    
+    def get_inf_inp(self):
+        """get inference input for deepagent"""
+        return {}
+
+    def get_deepagent_params(self):
+        """extra params to pass into the creation of deepagents"""
+        return {}
