ec6b6e45455c970fcf61f754af0b8712edfc4381
goulustis <cnimaiv@gmail.com>
2026-02-28 15:50:17 +0800

    add deepagent to registry

--- lang_agent/graphs/__init__.py
+++ lang_agent/graphs/__init__.py
@@ -4,12 +4,16 @@ from lang_agent.graphs.react import ReactGraphConfig, ReactGraph
 from lang_agent.graphs.routing import RoutingConfig, RoutingGraph
 from lang_agent.graphs.dual_path import DualConfig, Dual
 from lang_agent.graphs.vision_routing import VisionRoutingConfig, VisionRoutingGraph
+# from lang_agent.graphs.child_demo import ChildDemoGraphConfig, ChildDemoGraph
+from lang_agent.graphs.qt_deepagents import DeepAgentConfig
 
 graph_dict = {
     "react": ReactGraphConfig(),
     "route": RoutingConfig(),
     "dual": DualConfig(),
-    "vision": VisionRoutingConfig()
+    "vision": VisionRoutingConfig(),
+    # "child_demo": ChildDemoGraphConfig(),
+    "deepagent": DeepAgentConfig()
 }
 
 graph_union = tyro.extras.subcommand_type_from_defaults(graph_dict, prefix_names=False)
