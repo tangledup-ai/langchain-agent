b070d08130140f2965b347225f2e9f028e653b1a
goulustis <cnimaiv@gmail.com>
2026-03-04 16:05:13 +0800

    correct files

--- scripts/create_config.sh
+++ scripts/create_config.sh
@@ -3,10 +3,10 @@ conda init
 conda activate lang
 
 echo create blueberry config
-python scripts/py_scripts/misc_tasks.py --save-path config/pipelines/blueberry.yaml \
+python scripts/py_scripts/misc_tasks.py --save-path configs/pipelines/blueberry.yaml \
 										react \
 										--sys-prompt-f configs/prompts/blueberry.txt \
 			                 			--tool-manager-config.client-tool-manager.tool-keys
 
 # echo create xiaozhan config
-python scripts/py_scripts/misc_tasks.py --save-path config/pipelines/xiaozhan.yaml
+python scripts/py_scripts/misc_tasks.py --save-path configs/pipelines/xiaozhan.yaml
