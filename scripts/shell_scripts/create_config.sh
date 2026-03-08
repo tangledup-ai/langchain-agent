2d90a4caac106c27d0735cc30f608e8c9b505087
goulustis <cnimaiv@gmail.com>
2026-03-08 12:15:03 +0800

    update path

--- scripts/shell_scripts/create_config.sh
+++ scripts/shell_scripts/create_config.sh
@@ -1,3 +1,8 @@
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+PROJECT_ROOT="$(dirname "$(dirname "$SCRIPT_DIR")")"
+
+cd "$PROJECT_ROOT"
+
 source ~/.bashrc
 conda init
 conda activate lang
