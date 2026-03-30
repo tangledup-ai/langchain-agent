de8e0e49a8733ef64236a621e86759de03c0ad50
goulustis <cnimaiv@gmail.com>
2026-03-30 15:03:31 +0800

    feat: register agentbaysandbox backend in fs_bkends

--- lang_agent/fs_bkends/__init__.py
+++ lang_agent/fs_bkends/__init__.py
@@ -4,12 +4,21 @@ from lang_agent.fs_bkends.base import BaseFilesystemBackend
 from lang_agent.fs_bkends.statebk import StateBk, StateBkConfig
 from lang_agent.fs_bkends.localshell import LocalShell, LocalShellConfig
 from lang_agent.fs_bkends.daytona_sandbox import DaytonaSandboxBk, DaytonaSandboxConfig
+from lang_agent.fs_bkends.agentbay_sandbox import (
+    AgentBaySandboxBk,
+    AgentBaySandboxConfig,
+)
 
 statebk_dict = {
     "statebk": StateBkConfig(),
     "localshell": LocalShellConfig(),
     "daytonasandbox": DaytonaSandboxConfig(),
+    "agentbaysandbox": AgentBaySandboxConfig(),
 }
 
-statebk_union = tyro.extras.subcommand_type_from_defaults(statebk_dict, prefix_names=False)
-AnnotatedStateBk = tyro.conf.OmitSubcommandPrefixes[tyro.conf.SuppressFixed[statebk_union]]
+statebk_union = tyro.extras.subcommand_type_from_defaults(
+    statebk_dict, prefix_names=False
+)
+AnnotatedStateBk = tyro.conf.OmitSubcommandPrefixes[
+    tyro.conf.SuppressFixed[statebk_union]
+]
